2af38ea8050e7ec209c22912994ffe30d991498c
Kiran Kumar Vanam <kiran@Kirans-iMac.local>
2023-10-13 11:49:47 -0500

    Spring Web, Data JPA, MySQL Driver, DevTools

---
+++ src/main/java/in/peacock/peacockapi/PeacockApiApplication.java
@@ -0,0 +1,13 @@
+package in.peacock.peacockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeacockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeacockApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/peacock/peacockapi/PeacockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package in.peacock.peacockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeacockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
